5f50a76d894046cdf1d0d4abb755318d7310adce
Rokas <rookas.rudzenskas@gmail.com>
2023-04-21 19:32:20 +0300

    Add GPT data to CalloutCard message in WeatherPage

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -48,6 +48,9 @@ const WeatherPage = async ({params: {city, lat, long}}: Props) => {
     }
   })
 
+  const GPTdata = await res.json();
+  const { content } = GPTdata;
+
   return (
     <div className="flex flex-col min-h-screen md:flex-row">
       <InformationPanel city={city} results={results} lat={lat} long={long} />
@@ -60,7 +63,7 @@ const WeatherPage = async ({params: {city, lat, long}}: Props) => {
             </p>
           </div>
           <div className="m-2 mb-10">
-            <CalloutCard  message={"This is where the GPT will summarize the data"} />
+            <CalloutCard  message={content} />
           </div>
 
           <div className="grid grid-cols-1 xl:grid-cols-2 gap-x-5 gap-y-5 m-2">
