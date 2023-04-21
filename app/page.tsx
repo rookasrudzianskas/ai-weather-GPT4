eaf2db571deb993d287b679cc478bce46ad56baa
Rokas <rookas.rudzenskas@gmail.com>
2023-04-21 14:35:45 +0300

    Add max width to Card component on Home page in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import {Card, Divider, Subtitle, Text} from "@tremor/react";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center">
-      <Card>
+      <Card className="max-w-4xl mx-auto">
         <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
         <Subtitle className="text-xl text-center">Powered by Open AI, Next.js 13.2, Tailwind css, Tremor 2.0 and More!</Subtitle>
         <Divider className="my-10" />
