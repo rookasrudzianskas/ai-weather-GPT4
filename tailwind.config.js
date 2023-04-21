5c41a60d7aa889beaa1711618c420d4c1b140a70
Rokas <rookas.rudzenskas@gmail.com>
2023-04-21 14:29:14 +0300

    Add Tremor React package and configure server components external packages in next.config.js, and update dependencies in package.json and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    // Path to the tremor module
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
