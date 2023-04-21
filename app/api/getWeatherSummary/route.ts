f33bc206311346c0661d93c16568451997bef7a3
Rokas <rookas.rudzenskas@gmail.com>
2023-04-21 19:16:45 +0300

    Add weather summary POST route with instructions for presenting weather data as a news presenter. Also includes user message requesting weather summary with provided data

--- app/api/getWeatherSummary/route.ts
+++ app/api/getWeatherSummary/route.ts
@@ -12,8 +12,15 @@ export async function POST(request: Request) {
     messages: [
       {
         role: 'system',
-        content: "Pretend you're a weather news presenter presenting LIVE on television. be energetic and full of charisma. Introduce yourself as Rokas and say you are from the ByRookas HeadQuarters. State the city you are providing the summary for. Then give a summary of todays weather only. Make it easy for the viewer to understand and know what to do to prepare for those weather conditions such as wear SPF if the UV is high etc. use the uv_index data provided to provide UV advice. Provide a joke regarding the weather. Assume the data came from your team at the news office and not the user."
-
+        content: `Pretend you're a weather news presenter presenting LIVE on television. be energetic and full of charisma. 
+        Introduce yourself as Rokas and say you are from the ByRookas HeadQuarters. State the city you are providing the summary
+        for. Then give a summary of todays weather only. Make it easy for the viewer to understand and know what to do to prepare
+        for those weather conditions such as wear SPF if the UV is high etc. use the uv_index data provided to provide UV advice.
+        Provide a joke regarding the weather. Assume the data came from your team at the news office and not the user.`
+      },
+      {
+        role: 'user',
+        content: `Hi, there can i get summary of todays weather, use the following information to get the weather data: ${JSON.stringify(weatherData)}`
       }
     ]
   })
