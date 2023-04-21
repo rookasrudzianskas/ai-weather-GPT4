ea3b141ae4c1a0c258e5f3872edd610f51eb0f93
Rokas <rookas.rudzenskas@gmail.com>
2023-04-21 14:35:06 +0300

    Add Weather AI Card with Subtitle and City Picker component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 'use client'
 
+import {Card, Divider, Subtitle, Text} from "@tremor/react";
+
 export default function Home() {
   return (
-    <main className="text-6xl">
-      Lets build the future together
-    </main>
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center">
+      <Card>
+        <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+        <Subtitle className="text-xl text-center">Powered by Open AI, Next.js 13.2, Tailwind css, Tremor 2.0 and More!</Subtitle>
+        <Divider className="my-10" />
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          {/*  City Picker component */}
+        </Card>
+      </Card>
+    </div>
   )
 }
