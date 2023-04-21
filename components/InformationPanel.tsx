cdd4cdc02ab56c67b0c814154aa497eb8f099e7e
Rokas <rookas.rudzenskas@gmail.com>
2023-04-21 18:19:53 +0300

    Add sunrise information to InformationPanel component

--- components/InformationPanel.tsx
+++ components/InformationPanel.tsx
@@ -59,6 +59,22 @@ const InformationPanel = ({city, lat, long, results}: Props) => {
           </div>
         </div>
       </div>
+
+      <div>
+        <div className="flex items-center space-x-2 px-4 py-3 border border-[#6F90CD] rounded-md bg-[#405885]">
+          <SunIcon className="h-10 w-10 text-gray-400" />
+          <div className="flex-1 flex justify-between items-center">
+            <p className="font-extralight">Sunrise</p>
+            <p className="uppercase text-2xl">
+              {new Date(results.daily.sunrise[0]).toLocaleTimeString("en-GB", {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true
+              })}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
