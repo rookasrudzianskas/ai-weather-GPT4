3fd402115dc1499c3c0d807d340236aa5565a3e7
Rokas <rookas.rudzenskas@gmail.com>
2023-04-21 16:28:40 +0300

    Create Apollo client with URI, cache, and headers using environment variables

---
+++ apollo-client.ts
@@ -0,0 +1,13 @@
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+let client: ApolloClient<any> | null = null;
+
+export const getClient = () => {
+  const client = new ApolloClient({
+    uri: process.env.API_URL,
+    cache: new InMemoryCache(),
+    headers: {
+      Authorization: `apikey ${process.env.NEXT_PUBLIC_API_KEY}`
+    }
+  })
+}
