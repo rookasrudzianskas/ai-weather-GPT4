d2a5bc8270f6d584260b0fbbc1cbdd44b7b95eb0
Rokas <rookas.rudzenskas@gmail.com>
2023-04-21 14:54:42 +0300

    Add react-select to CityPicker component to enable user-friendly country selection

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -2,11 +2,23 @@
 
 import React from 'react';
 import { Country, State, City }  from 'country-state-city';
+import Select from "react-select";
+
+const options = Country.getAllCountries().map((country) => ({
+  value: {
+    lat: country.latitude,
+    lng: country.longitude,
+    iso: country.isoCode,
+  },
+  label: country.name,
+}))
 
 const CityPicker = ({}) => {
   return (
     <div>
-      CityPicker
+      <Select
+        options={options}
+      />
     </div>
   );
 };
