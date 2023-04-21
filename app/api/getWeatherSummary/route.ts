7a440b71bc0d28b632b9a2437db21009a26db70f
Rokas <rookas.rudzenskas@gmail.com>
2023-04-21 19:14:49 +0300

    Add OpenAI library import and create a chat completion response to generate a weather news summary with UV advice and a joke

--- app/api/getWeatherSummary/route.ts
+++ app/api/getWeatherSummary/route.ts
@@ -1,3 +1,20 @@
-export async function POST(request: Request) {
+import openai from "@/openai";
 
+export async function POST(request: Request) {
+  // will be in the body of post request
+  const { weatherData } = await request.json();
+
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    temperature: 0.8,
+    n: 1,
+    stream: false,
+    messages: [
+      {
+        role: 'system',
+        content: "Pretend you're a weather news presenter presenting LIVE on television. be energetic and full of charisma. Introduce yourself as Rokas and say you are from the ByRookas HeadQuarters. State the city you are providing the summary for. Then give a summary of todays weather only. Make it easy for the viewer to understand and know what to do to prepare for those weather conditions such as wear SPF if the UV is high etc. use the uv_index data provided to provide UV advice. Provide a joke regarding the weather. Assume the data came from your team at the news office and not the user."
+
+      }
+    ]
+  })
 }
