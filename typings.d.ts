16a531c9bdf58bff2116b61fd0917767e25e165a
Rokas <rookas.rudzenskas@gmail.com>
2023-04-21 16:18:59 +0300

    Add interfaces for CurrentWeather, Daily, DailyUnits, and Hourly to typings.d.ts file

---
+++ typings.d.ts
@@ -0,0 +1,49 @@
+interface CurrentWeather {
+  temperature: number;
+  time: string;
+  weathercode: number;
+  winddirection: number;
+  windspeed: number;
+}
+
+interface Daily {
+  apparent_temperature_max: [number];
+  apparent_temperature_min: [number];
+  sunrise: [string];
+  sunset: [string];
+  temperature_2m_max: [number];
+  temperature_2m_min: [number];
+  time: [string];
+  uv_index_clear_sky_max: [number];
+  uv_index_max: [number];
+  weathercode: [number];
+}
+
+interface DailyUnits {
+  apparent_temperature_max: string;
+  apparent_temperature_min: string;
+  sunrise: string;
+  sunset: string;
+  temperature_2m_max: string;
+  temperature_2m_min: string;
+  time: string;
+  uv_index_clear_sky_max: number;
+  uv_index_max: number;
+  weathercode: number;
+}
+
+interface Hourly {
+  apparent_temperature: [number];
+  precipitation: [number];
+  precipitation_probability: [number];
+  rain: [number];
+  relativehumidity_2m: [number];
+  showers: [number];
+  snow_depth: [number];
+  snowfall: [number];
+  temperature_2m: [number];
+  time: [string];
+  uv_index: [number];
+  uv_index_clear_sky: [number];
+  windgusts_10m: [number];
+}
