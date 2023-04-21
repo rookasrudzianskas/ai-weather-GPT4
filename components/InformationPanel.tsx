2caad4564292ed1acf56af424ef7d30a5a342a82
Rokas <rookas.rudzenskas@gmail.com>
2023-04-21 17:26:58 +0300

    Add InformationPanel component with city, lat, long, and results props

---
+++ components/InformationPanel.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {MoonIcon, SunIcon} from "@heroicons/react/solid";
+
+type Props = {
+  city: string;
+  lat: string;
+  long: string;
+  results: Root;
+}
+
+const InformationPanel = ({city, lat, long, results}: Props) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default InformationPanel;
+// by Rokas with ❤️
